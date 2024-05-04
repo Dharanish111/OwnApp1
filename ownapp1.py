c6092b401498a117c8e2f90214503c71d69b98b1
Dharanish111 <168800443+Dharanish111@users.noreply.github.com>
2024-05-04 21:36:44 +0530

    Add files via upload

--- ownapp1.py
+++ ownapp1.py
@@ -54,15 +54,18 @@ if st.button('Start'):
 else:
      st.write('wanna cook')
 	
-st.title('st.progress')
+st.title('Baking')
 my_bar = st.progress(0)
 for percent_complete in range(100):
      time.sleep(0.05)
      my_bar.progress(percent_complete + 1)
 	
 st.title('Biryani')
+
+st.header('Insert Biryani pic')
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not none:
+
+if uploaded_file is not None:
 	df = pd.read_csv(uploaded_file)
 	st.subheader('DataFrame')
 	st.write(df)
